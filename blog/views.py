d6ee99473454637397fc9e8c8f35defd52758ee3
ViktorVangok <62500742+ViktorVangok@users.noreply.github.com>
2021-09-20 22:13:42 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,19 @@ from django.views.generic import ListView, DetailView, CreateView
 from blog.models import Post, Comment
 from .forms import CommentForm
 
+from multiprocessing import Process
+import logging
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('cnf.ini')
+logging.basicConfig(
+    level=config['LOGGING']['level'],
+    filename=config['LOGGING']['filename']
+)
+log = logging.getLogger(__name__)
+
 
 class HomeView(ListView):
     model = Post
@@ -24,6 +37,7 @@ class PostDetailView(DetailView):
     slug_url_kwarg = 'post_slug'
 
     def get_context_data(self, **kwargs):
+        p = Process(target=log.info(f'Added {super().get_context_data(**kwargs)}'))
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         return context
